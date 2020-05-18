3cf05bffe25806abfe3f9a55ddc130c3b04a1291
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-18 09:30:15 +0900

    Update home.php

--- home.php
+++ home.php
@@ -1,43 +1,20 @@
 <?php
 session_start();
- ?>
- <!DOCTYPE html>
- <html lang="ja" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title>ファイルアップローダ</title>
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+require_once("smarty/libs/Smarty.class.php");
+$smarty = new Smarty();
 
-   </head>
-   <body>
-<?php
+$smarty->template_dir = './templates/';
+$smarty->compile_dir = './templates_c/';
+
+$e=1;
 
 if(isset($_SESSION['us']) && $_SESSION['us'] != null){
-?>
-<div class="container">
-  <h5 class = " my-5">アップロードするファイルを選んでください</h5>
-    <form  action="setup.php" method="post" enctype="multipart/form-data">
-      <div class="input-group col-9">
-        <div class="custom-file">
-          <input type="file" name = "myf" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
-          <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
-        </div>
-        <div class="input-group-append">
-          <button class="btn btn-outline-primary ml-5" type="submit" id="inputGroupFileAddon04">アップロード</button>
-        </div>
-      </div>
-    </form>
-
-</div>
-
-<?php
+ $smarty->assign("upload_disp",$e);
 }else{
-  session_destroy();
-  print "<a href = 'logon.html'>ログインして下さい</a>";
-
+ $smarty->assign("upload_disp",'');
+ session_destroy();
 }
+
+$smarty->display("home.tpl");
  ?>
-</div>
-   </body>
- </html>
